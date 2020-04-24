8f1623ce7561d53ddc5b72bcc567102a0a01d86b
dang.hh <hohaidang.khtn@gmail.com>
2020-04-24 09:44:01 +0900

    Add

---
+++ AddBinary/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
+
+
+a = Solution()
+a.addBinary("1001", "1111")
